ed3a168796b2b7c0b704f8018b5631d86070dcea
r3r00t3d <r3r00t3d@gmail.com>
2019-07-25 00:40:06 +0200

    Added support for testing and Travis CI

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+/* eslint-disable no-undef */
 const express = require('express')
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+
+app.listen(port, () => {
+    console.log('Server is run on', port)
+})
