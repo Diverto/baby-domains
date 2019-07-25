0909aa114c8df846a46c25a856e4ebb145c3cc81
r3r00t3d <r3r00t3d@gmail.com>
2019-07-25 14:59:09 +0200

    Major overhaul of folders and Docker data

--- server/src/index.js
+++ server/src/index.js
@@ -6,6 +6,10 @@ const app = express()
 const port = process.env.PORT || 3000
 
 
+app.get('/', (req, res) => {
+    res.status(200).send('Hi!')
+})
+
 app.listen(port, () => {
     console.log('Server is run on', port)
 })
