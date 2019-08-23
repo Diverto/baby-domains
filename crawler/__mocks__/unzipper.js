ea2dfacb53e000257cec9232a2c667a443673cda
tandros-div <tomislav.andros@diverto.hr>
2019-08-23 14:18:34 +0200

    Fixed some tests

--- crawler/__mocks__/unzipper.js
+++ crawler/__mocks__/unzipper.js
@@ -1,19 +1,24 @@
-var path = require('path');
-var stream = require('stream');
-
+// var path = require('path');
+// var stream = require('stream');
+let logger = require('./src/logger_dev').logger
 
 exports.Extract = (opts) => {
+    logger.debug(opts)
     // make sure path is normalized before using it
-    opts.path = path.normalize(opts.path)
+    // opts.path = path.normalize(opts.path)
     
-    let mockedStream = new stream.Writable({objectMode: true})
-    mockedStream._write = (_entry, _encoding, cb) => {
-        cb()
-    }
-    mockedStream.emit('close')
+    // let mockedStream = new stream.Writable({objectMode: true})
+    // mockedStream._write = (_entry, _encoding, cb) => {
+    //     cb()
+    // }
+    // mockedStream.emit('close')
   
-    return new Promise((resolve, reject) => {
-      mockedStream.on('close', resolve)
-      mockedStream.on('error',reject);
+    // return new Promise((resolve, reject) => {
+    //   mockedStream.on('close', resolve)
+    //   mockedStream.on('error',reject);
+    // })
+    return new Promise((resolve) => {
+        resolve()
     })
+
 }
