ada75d644b52a029e937ff07bdd2e7e17ad7fd84
tandros-div <tomislav.andros@diverto.hr>
2019-09-12 10:39:55 +0200

    Added support for mongodb and index.js file

--- api-creator/src/index.js
+++ api-creator/src/index.js
@@ -1,20 +1,16 @@
 /**
- * @module crawler
+ * @module api-creator
  */
 var CronJob = require('cron').CronJob
 const mongoConnect = require('./db/mongoose').mongoConnect
 const mongoClose = require('./db/mongoose').mongoClose
-const fetchStoreZippedDomainFile = require('./scraper').fetchStoreZippedDomainFile
-const parseDomainsAndStore = require('./parseAndStore').parseDomainsAndStore
 const logger = require('./logger')
 
-// ESLint-happy IIFE
 !async function main() {
     try {
-        new CronJob('0 18 * * *', async function() {
+        new CronJob('0 21 * * *', async function() {
             const { db } = await mongoConnect()
-            const { dateRegistered, dateFilename } = await fetchStoreZippedDomainFile();
-            await parseDomainsAndStore({dateRegistered, dateFilename})
+           
             await mongoClose(db)
           }, null, true, 'Europe/Vienna', undefined, true);
     } catch (e) {
---
+++ api-creator/src/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mongoUser: process.env.MONGOUSER,
+    mongoHost: process.env.MONGOHOST,
+    mongoDatabase: process.env.MONGODATABASE,
+    mongoPassword: process.env.MONGOPASSWORD,
+    mongoPort: process.env.MONGOPORT,
+    mongoProtocol: process.env.MONGOPROTOCOL,
+    nodeEnv: process.env.NODE_ENV
+  };
