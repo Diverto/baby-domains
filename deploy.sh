3801e426a59adec5977ead740e87bf0bb0d80350
r3r00t3d <r3r00t3d@gmail.com>
2019-12-02 21:09:18 +0000

    Try fix with stateful sets #7

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,6 @@ docker push tandrosdiv/baby-api-creator:$SHA
 
 kubectl apply -f k8s
 
-kubectl set image deployments/server-deployment server=tandrosdiv/baby-api:$SHA
+kubectl set image statefulset webserver webserver=tandrosdiv/baby-api:$SHA
 kubectl set image deployments/crawler-deployment crawler=tandrosdiv/baby-crawler:$SHA
-kubectl set image deployments/api-creator-deployment api-creator=tandrosdiv/baby-api-creator:$SHA
+kubectl set image statefulset api-creator api-creator=tandrosdiv/baby-api-creator:$SHA
